c16404ecd1545f56ab33283855be6bb878b4e9fd
Décio Machado <deciomachado90@gmail.com>
2019-10-11 22:39:35 +0000

    Added Player Class

--- src/org/academiadecodigo/thunderstructs/Player.java
+++ src/org/academiadecodigo/thunderstructs/Player.java
@@ -1,17 +1,48 @@
 package org.academiadecodigo.thunderstructs;
 
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
-public class Player {
+public class Player implements KeyboardHandler {
     public static final int PADDING = 10;
     private Picture player;
 
+
     public Player() {
-        player = new Picture(PADDING,PADDING,"sprites/background_1.jpg");
+        player = new Picture(50,300,"sprites/player/player_idle.png");
     }
 
     public void init() {
         player.draw();
         player.grow(0,0);
+    }
+
+    public int getX(){
+
+        return player.getX();
+    }
+
+    public int getY(){
+
+        return player.getY();
+    }
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+        switch (keyboardEvent.getKey()){
+            case KeyboardEvent.KEY_LEFT:
+                player.translate(-10,0);
+                player.draw();
+                break;
+            case KeyboardEvent.KEY_RIGHT:
+                player.translate(10,0);
+                player.draw();
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+
     }
 }
