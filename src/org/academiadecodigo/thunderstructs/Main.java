110a290d2b781e4cc6db45e928c111fe8b53e6ef
Rafael Azevedo <r95@live.com.pt>
2019-10-11 20:04:56 +0000

    Basic Structure

---
+++ src/org/academiadecodigo/thunderstructs/Main.java
@@ -0,0 +1,16 @@
+package org.academiadecodigo.thunderstructs;
+
+import org.academiadecodigo.thunderstructs.game.Game;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Game game = new Game();
+
+        game.start();
+
+
+    }
+
+}
---
+++ src/org/academiadecodigo/thunderstructs/game/Game.java
@@ -0,0 +1,15 @@
+package org.academiadecodigo.thunderstructs.game;
+
+public class Game {
+
+    public Game() {
+
+    }
+
+    public void start() {
+
+    }
+
+
+
+}
