51ca0a4a46e9932530268c84cfb47f6499985b64
Décio Machado <deciomachado90@gmail.com>
2019-10-12 00:14:59 +0000

    Added mapWidth parameter on player constructor

--- src/org/academiadecodigo/thunderstructs/Player.java
+++ src/org/academiadecodigo/thunderstructs/Player.java
@@ -3,14 +3,17 @@ package org.academiadecodigo.thunderstructs;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
+import org.academiadecodigo.thunderstructs.map.Map;
 
 public class Player implements KeyboardHandler {
-    public static final int PADDING = 10;
+
     private Picture player;
+    private int mapWidth;
 
 
-    public Player() {
+    public Player(int mapWidth) {
         player = new Picture(50,300,"sprites/player/player_idle.png");
+        this.mapWidth = mapWidth;
     }
 
     public void init() {
@@ -27,14 +30,20 @@ public class Player implements KeyboardHandler {
 
         return player.getY();
     }
+
+
     @Override
     public void keyPressed(KeyboardEvent keyboardEvent) {
         switch (keyboardEvent.getKey()){
             case KeyboardEvent.KEY_LEFT:
+                if(player.getX() < Map.PADDING){
+                    return;
+                }
                 player.translate(-10,0);
                 player.draw();
                 break;
             case KeyboardEvent.KEY_RIGHT:
+                //if(player.getX() > Map.)
                 player.translate(10,0);
                 player.draw();
                 break;
