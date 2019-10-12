0a42dcf7876aef630ad4e75fb53511edb1409b0c
Vasco Sousa <az.thewolf.jr@gmail.com>
2019-10-12 20:42:57 +0000

    Implemented tile disappear method

--- src/org/academiadecodigo/thunderstructs/objects/Floor.java
+++ src/org/academiadecodigo/thunderstructs/objects/Floor.java
@@ -8,7 +8,7 @@ public class Floor extends Map {
     private Picture floorTile;
 
     private int mapWidth;
-    private int cellsize;
+    private int cellSize;
 
     private Picture[] tiles;
 
@@ -16,30 +16,44 @@ public class Floor extends Map {
         this.mapWidth = mapWidth;
         floorTile = new Picture(Map.PADDING - 100, 351, "resources/sprites/blockTexture.png");
         floorTile.grow(-100, -100);
-        cellsize = floorTile.getWidth();
-        tiles = new Picture[33 * cellsize];
+        cellSize = floorTile.getWidth();
+        tiles = new Picture[33 * cellSize];
     }
 
     public void init() {
         tileFloor();
     }
 
-    public Picture getPicture() {
-        return this.floorTile;
-    }
-
     public void tileFloor() {
 
-        System.out.println(cellsize);
-        for (int i = 0; i < mapWidth; i += cellsize) {
+        System.out.println(cellSize);
+        for (int i = 0; i < mapWidth; i += cellSize) {
             //tiles[i] = floorTile;
             tiles[i] = new Picture(i - 90, 351, "resources/sprites/blockTexture.png");
             tiles[i].grow(-100, -100);
             tiles[i].draw();
-            //System.out.println(floorTile.getY());
         }
-        //floorTile.translate(20,0);
-        //floorTile.draw();floorTile.draw();
+    }
+
+    public void openTile() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ie) {
+            System.out.println(ie);
+        }
+
+        tiles[5].delete();
+        tiles[6].delete();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ie) {
+            System.out.println(ie);
+        }
+
+
+        tiles[5].draw();
+        tiles[6].draw();
     }
 
 
