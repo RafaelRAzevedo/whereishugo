500f5991f9d595833d36e50dcfcecea477250c40
Rafael Azevedo <r95@live.com.pt>
2019-10-12 00:18:48 +0000

    Added Falling rock class

---
+++ src/org/academiadecodigo/thunderstructs/objects/FallingRock.java
@@ -0,0 +1,11 @@
+package org.academiadecodigo.thunderstructs.objects;
+
+public class FallingRock {
+
+    private int mapHeight;
+
+    public FallingRock(int mapHeight) {
+        this.mapHeight = mapHeight;
+    }
+
+}
